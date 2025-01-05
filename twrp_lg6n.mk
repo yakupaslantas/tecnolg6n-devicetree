2fe52dd710437ed4115a0ac1bd8c40b45c21cc52
yakupaslantas <martyrerdixon@gmail.com>
2025-01-05 03:20:54 +0300

    Update and rename twrp_LG6n.mk to twrp_lg6n.mk

--- twrp_lg6n.mk
+++ twrp_lg6n.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from TECNO-LG6n device
-$(call inherit-product, device/tecno/TECNO-LG6n/device.mk)
+$(call inherit-product, device/tecno/lg6n/device.mk)
 
-PRODUCT_DEVICE := TECNO-LG6n
-PRODUCT_NAME := twrp_TECNO-LG6n
+PRODUCT_DEVICE := lg6n
+PRODUCT_NAME := twrp_lg6n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LG6n
 PRODUCT_MANUFACTURER := tecno
