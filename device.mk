e9b45c2928ef2a08183a2a2bac0c312b140c0e89
yakupaslantas <martyrerdixon@gmail.com>
2025-01-05 03:22:56 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/tecno/TECNO-LG6n
+LOCAL_PATH := device/tecno/lg6n
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
