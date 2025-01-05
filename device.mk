8a4412384b0466895c9db69988956aa99d2b33e5
yakupaslantas <martyrerdixon@gmail.com>
2025-01-05 03:26:20 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6768
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6768 \
     libgptutils \
     libz \
